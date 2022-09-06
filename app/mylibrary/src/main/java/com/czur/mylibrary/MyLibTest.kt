73e1120be3a2343403f41f39fb151f339ffda63a
username@email.com <51082006@sohu.com>
2022-09-06 17:22:41 +0800

    second commit

---
+++ app/mylibrary/src/main/java/com/czur/mylibrary/MyLibTest.kt
@@ -0,0 +1,16 @@
+package com.czur.mylibrary
+
+public class MyLibTest {
+
+    public fun calculation(number: Int): Int {
+        return number * number
+    }
+
+    //echo "# myLibTest" >> README.md
+    //git init
+    //git add README.md
+    //git commit -m "first commit"
+    //git branch -M main
+    //git remote add origin https://github.com/jason51082006/myLibTest.git
+    //git push -u origin main
+}
